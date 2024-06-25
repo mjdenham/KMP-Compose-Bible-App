350b2381f157fbb74e53512ff72ab572f8595b10
mjdenham <mjdenham@gmail.com>
2024-06-25 18:58:33 +0100

    Initial commit

---
+++ composeApp/src/commonMain/kotlin/Greeting.kt
@@ -0,0 +1,10 @@
+import kotlin.random.Random
+
+class Greeting {
+    private val platform = getPlatform()
+
+    fun greet(): String {
+        val firstWord = if (Random.nextBoolean()) "Hi" else "Hello"
+        return "$firstWord, ${platform.name}!"
+    }
+}
