4abc32c5724b65c965c25e0a319c955b58963b72
mjdenham <mjdenham@gmail.com>
2024-06-25 18:58:33 +0100

    Use material 3

--- composeApp/src/commonMain/kotlin/com/martin/bibleapp/SearchScreen.kt
+++ composeApp/src/commonMain/kotlin/com/martin/bibleapp/SearchScreen.kt
@@ -6,8 +6,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Scaffold
-import androidx.compose.material.TextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
