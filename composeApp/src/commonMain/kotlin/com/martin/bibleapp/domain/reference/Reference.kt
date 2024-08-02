c250bade3b87fb981c8e611b680caec27b27f45b
mjdenham <mjdenham@gmail.com>
2024-08-02 19:29:47 +0100

    Add verse to reference; default to John 3:16

--- composeApp/src/commonMain/kotlin/com/martin/bibleapp/domain/reference/Reference.kt
+++ composeApp/src/commonMain/kotlin/com/martin/bibleapp/domain/reference/Reference.kt
@@ -1,11 +1,11 @@
 package com.martin.bibleapp.domain.reference
 
-data class Reference(val book: BibleBook, val chapter: Int = 1) {
+data class Reference(val book: BibleBook, val chapter: Int = 1, val verse: Int = 1) {
     fun shortLabel() = "${book.shortLabel()} $chapter"
-    fun referenceCode() = toCode(book, chapter)
+    fun referenceCode() = toCode(book, chapter, verse)
 
     companion object {
-        val DEFAULT = Reference(BibleBook.JOHN, 3)
+        val DEFAULT = Reference(BibleBook.JOHN, 3, 16)
 
         fun toCode(book: BibleBook, chapter: Int, verse: Int): String = "${book.usfmCode}.$chapter.$verse"
         fun toCode(book: BibleBook, chapter: Int): String = "${book.usfmCode}.$chapter"
